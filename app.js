11f51ef2d663903ae301dcfe6fc129f5ae80b67e
hulloanson <hulloanson@gmail.com>
2023-09-13 01:37:01 -0400

    save wrong answers and added page to review them

--- app.js
+++ app.js
@@ -3,19 +3,23 @@ const bodyParser = require("body-parser");
 const https = require("https");
 const fs = require("fs");
 
-
 const app = express();
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/", function(req,res) {
-  res.sendFile(__dirname + "/test.html")
+app.get("/review-wrong.:ext", function(req, res) {
+  res.sendFile(__dirname + `/review-wrong.${req.params.ext}`);
+});
+
+app.use("/public", express.static("public"));
+
+app.post("/", function(req, res) {
+  res.sendFile(__dirname + "/test.html");
 });
 
 app.post("/game", function(req, res) {
---
+++ public/storage.js
@@ -0,0 +1,43 @@
+const WRONGLY_ANSWERED_KEY = "wronglyAnswered";
+function AnswersStore() {
+  // load wrongly answered
+  try {
+    const stored = localStorage.getItem(WRONGLY_ANSWERED_KEY);
+    console.log("stored:", stored);
+    if (!stored) throw new Error(WRONGLY_ANSWERED_KEY + " does not exist");
+    else this.wronglyAnswered = JSON.parse(stored);
+  } catch (e) {
+    // JSON parse failed / null, load an empty array
+    this.clear();
+  }
+}
+
+AnswersStore.prototype.clear = function clear() {
+  console.log("cleared.");
+  this.wronglyAnswered = [];
+  localStorage.removeItem(WRONGLY_ANSWERED_KEY);
+};
+
+AnswersStore.prototype.findAlreadyWrong = function findAlreadyWrong(question) {
+  return (
+    this.wronglyAnswered.filter(
+      (answered) => answered.question.question === question.question
+    ).length > 0
+  );
+};
+
+AnswersStore.prototype.saveWrongAnswer = function saveWrongAnswer(
+  question,
+  answer
+) {
+  if (this.findAlreadyWrong(question)) return;
+  this.wronglyAnswered.push({ question, answer });
+  localStorage.setItem(
+    WRONGLY_ANSWERED_KEY,
+    JSON.stringify(this.wronglyAnswered)
+  );
+};
+
+AnswersStore.prototype.getWronglyAnswered = function getWronglyAnswered() {
+  return this.wronglyAnswered;
+};
